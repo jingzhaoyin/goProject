8bd65b19d0f124b9b8e71e997109ecb51f3f9e44
jingzhaoyin <1319823672@qq.com>
2025-11-13 01:37:57 +0800

    //指针模块

--- src/two_project/workOne.go
+++ src/two_project/workOne.go
@@ -1,18 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "github.com/joho/godotenv"
-    "os"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+// 指针
 func main() {
-    // 加载.env文件中的环境变量
-    err := godotenv.Load()
-    if err != nil {
-        fmt.Println("Error loading .env file")
-    }
-    // 使用环境变量
-    myVar := os.Getenv("MY_ENV_VAR")
-    fmt.Println("My Environment Variable:", myVar)
+	// 加载.env文件中的环境变量
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	// 使用环境变量
+	myVar := os.Getenv("MY_ENV_VAR")
+	fmt.Println("My Environment Variable:", myVar)
 }
